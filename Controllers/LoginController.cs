9a71227ca3592a16f332dce750e5de3f8c9ffa94
ErianSerna <erianserna1114297@correo.itm.edu.co>
2025-04-22 00:59:56 -0500

    Se añade la validación por token y se conecta la base de datos

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Natillera_Parcial.Clases;
+using Natillera_Parcial.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace Natillera_Parcial.Controllers
+{
+    [RoutePrefix("api/login")]
+    [AllowAnonymous]
+
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("Ingresar")]
+        public IQueryable<LoginRespuesta> Ingresar([FromBody] Login login)
+        {
+            clsLogin _Login = new clsLogin();
+            _Login.login = login;
+            return _Login.Ingresar();
+        }
+    }
+}
